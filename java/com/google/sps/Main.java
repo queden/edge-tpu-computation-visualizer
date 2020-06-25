fe91960a44f349351e91be552c82cfeb8de3726e
Caden <cadenmfelton@gmail.com>
2020-06-25 21:00:12 +0000

    Real init commit

---
+++ java/com/google/sps/Main.java
@@ -0,0 +1,14 @@
+
+package com.google.sps;
+
+import java.util.HashTable;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello world!");
+    }
+
+    private static HashTable relateTensorsToInstructions(List<Allocation> allocations, List<InstructionTag> instructionTags) {
+        
+    }
+}
