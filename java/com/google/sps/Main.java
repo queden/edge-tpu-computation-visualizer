a809282413a3e540fa6da40694b5f851c14dbcd3
KidiH <kalemu@college.harvard.edu>
2020-06-26 14:46:05 +0000

    minor changes

--- java/com/google/sps/Main.java
+++ java/com/google/sps/Main.java
@@ -3,22 +3,26 @@ package com.google.sps;
 import java.util.HashTable;
 import java.util.ArrayList;
 import java.util.List;
-import com.google.sps.ValidationProto;
+import com.google.sps.SimulationTraceProto;
 
 public class Main {
-    HashTable<Allocation, new ArrayList<InstructionTag>()> tensorToInstructions;
+    ArrayList<TensorAllocation> tensorAddress = new ArrayList();
+   // HashTable<Allocation, new ArrayList<InstructionTag>()> tensorToAddress;
     public static void main(String[] args) {
       System.out.println("Hello world!");
+      SimulationTraceProto.TraceAllocation.Builder tensorAllo = new SimulationTraceProto.TraceAllocation.Builder;
+      SimulationTraceProto.TraceAllocation.Builder alloBuilder = new SimulationTraceProto.Allocation.Builder;
+      SimulationTraceProto.Instruction.Builder instructBuilder = new SimulationTraceProto.Instruction.Builder;
 
-      ValidationProto.Allocation.Builder alloBuilder = new ValidationProto.Allocation.Builder;
-      ValidationProto.InstructionTag.Builder instructBuilder = new ValidationProto.InstructionTag.Builder;
-
-      tensorToInstructions = relateTensorsToInstructions(alloBuilder.getTensorAllocationList(), instructBuilder.getInstructionTagList());
+      tensorToInstructions = relateTensorsToInstructions(alloBuilder.getTensorAllocationList(), instructBuilder.getInstructionList());
+      
     }
 
     private static HashTable relateTensorsToInstructions(List<Allocation> allocations, List<InstructionTag> instructionTags) {
         for (Allocation allocation : allocations) {
-            tensorToInstructions
+            
+            tensorToInstructions;
+            
         }
 
         for (InstructionTag instruction : instructionTags) {
