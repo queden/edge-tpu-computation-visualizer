38e7d6e3c16721ac9a9e338a0479ff6f52a16827
TahleeJ <tahlee.jaynes@gmail.com>
2020-07-01 10:53:39 -0400

    Add new tensor access exception and tensor read method

--- src/main/java/com/google/sps/data/TJCode.java
+++ src/main/java/com/google/sps/data/TJCode.java
@@ -1,10 +1,14 @@
 package com.google.sps.data;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Hashtable;
 import java.util.List;
 import com.google.sps.proto.SimulationTraceProto;
 import com.google.sps.proto.SimulationTraceProto.Instruction;
+import com.google.sps.proto.SimulationTraceProto.TraceEntry;
+import com.google.sps.proto.SimulationTraceProto.MemoryAccess;
+import com.google.sps.exceptions.*;
 
 public class TJCode {
     // Must be commented out for testing purposes
@@ -19,5 +23,50 @@ public class TJCode {
         }
 
         return instructionTagtoInstruction;
+    }
+
+    private static int[][] narrowMemory = new int[16][128 * 1024];
+    private static int[][] wideMemory = new int[16][256 * 1024];
+
+    public static void readValidation(List<TraceEntry> traces, Map<Integer, Instruction> tagToInstructions) {
+        Boolean[] maskList;
+        MemoryAccess memoryAccess;
+        Instruction instruction;
+        int tensor;
+        int address;
+
+        for (TraceEntry trace : traces) {
+            instruction = tagToInstructions.get(trace.getInstructionTag());
+
+            if (trace.getAccessType() == TraceEntry.AccessType.READ_NARROW) {
+                memoryAccess = instruction.getNarrowRead();
+                maskList = (Boolean[]) Arrays.asList(instruction.getMaskList()).toArray();
+
+                for (int tile = 0; tile < 16; tile++) {
+                    if (maskList[tile]) {
+                        tensor = memoryAccess.getTensor();
+                        address = trace.getAddress();
+
+                        if (narrowMemory[tile][address] != tensor) {
+                            throw new InvalidTensorReadException(tensor, tile, address, narrowMemory[tile][address], "narrow");
+                        }
+                    }
+                }
+            } else if (trace.getAccessType() == TraceEntry.AccessType.READ_WIDE) {
+                memoryAccess = instruction.getWideRead();
+                maskList = (Boolean[]) Arrays.asList(instruction.getMaskList()).toArray();
+
+                for (int tile = 0; tile < 16; tile++) {
+                    if (maskList[tile]) {
+                        tensor = memoryAccess.getTensor();
+                        address = trace.getAddress();
+
+                        if (wideMemory[tile][address] != tensor) {
+                            throw new InvalidTensorReadException(tensor, tile, address, wideMemory[tile][address], "wide");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ src/main/java/com/google/sps/exceptions/InvalidTensorReadException.java
@@ -0,0 +1,35 @@
+package com.google.sps.exceptions;
+
+import com.google.sps.data.*;
+
+public class InvalidTensorReadException extends Exception {
+    private int tensor;
+    private int tile;
+    private int address;
+    private int invalidTensor;
+    private String memory;
+
+    public InvalidTensorReadException(int tensor, int tile, int address, int invalidTensor, String memory) {
+        super();
+        this.tensor = tensor;
+        this.tile = tile;
+        this.address = address;
+        this.invalidTensor = invalidTensor;
+        this.memory = memory;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Tried to read tensor: " +
+            tensor +
+            " at " +
+            memory +
+            "memory location (tile: " +
+            tile +
+            " address: " +
+            address +
+            ") but was: " +
+            invalidTensor +
+            ".";
+    }
+}
