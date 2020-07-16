ba4d1c95b44d6b492a7282973ac3904f02273867
TahleeJ <tahlee.jaynes@gmail.com>
2020-07-16 15:54:27 +0000

    Structure changes

--- src/main/java/com/google/sps/servlets/VisualizerServlet.java
+++ src/main/java/com/google/sps/servlets/VisualizerServlet.java
@@ -9,6 +9,7 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import com.google.protobuf.TextFormat;
+import com.google.sps.data.FileJson;
 import com.google.sps.proto.SimulationTraceProto.*;
 import java.io.IOException;
 import java.io.InputStream;
