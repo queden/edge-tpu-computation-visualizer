edf84cc8243d6890cdfa9ea837c8fbf2da95dece
Caden <cadenmfelton@gmail.com>
2020-07-29 12:44:13 +0000

    Send allocation information

--- src/main/java/com/google/sps/results/PreProcessResults.java
+++ src/main/java/com/google/sps/results/PreProcessResults.java
@@ -1,5 +1,7 @@
 package com.google.sps.results;
 
+import com.google.sps.proto.MemaccessCheckerDataProto.*;
+
 public class PreProcessResults {
   public boolean isError;
   public String message;
@@ -7,13 +9,17 @@ public class PreProcessResults {
   public int numTiles;
   public int narrowSize;
   public int wideSize;
+  List<TensorLayerAllocationTable> tensorAllocationsNarrow;
+  List<TensorLayerAllocationTable> tensorAllocationWide;
 
-  public PreProcessResults(boolean isError, String message, int numTraces, int numTiles, int narrowSize, int wideSize) {
+  public PreProcessResults(boolean isError, String message, int numTraces, int numTiles, int narrowSize, int wideSize, List<TensorLayerAllocationTable> tensorAllocationsNarrow, List<TensorLayerAllocationTable> tensorAllocationWide) {
     this.isError = isError;
     this.message = message;
     this.numTraces = numTraces;
     this.numTiles = numTiles;
     this.narrowSize = narrowSize;
     this.wideSize = wideSize;
+    this.tensorAllocationNarrow = tensorAllocationNarrow;
+    this.tensorAllocationWide = tensorAllocationWide;
   }
 }
